c1e201f2f30d20e6fadc82aa7b593ae426c32e9a
Doxygen CI <ci-arduino@invalid>
2026-07-21 13:20:38 +0000

    Deploy docs to GitHub Pages from commit bf11c39134
    Commit: bf11c39134ed53089f58bdd2ae55f54cc2e73140
    GitHub Actions run: 29833671380

--- html/search/all_3.js
+++ html/search/all_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['getavailablecurrent',['getAvailableCurrent',['../class_adafruit___c_h224.html#a00f358650d9a661ef5786396398e8bd6',1,'Adafruit_CH224']]],
   ['getmaxcurrent',['getMaxCurrent',['../class_adafruit___c_h224.html#a94cdefd264a5190861dfde082e21757b',1,'Adafruit_CH224']]],
   ['getstatus',['getStatus',['../class_adafruit___c_h224.html#a4513d66ba52d6e11a6634e06c8b7fabf',1,'Adafruit_CH224']]]
 ];
--- html/search/all_4.js
+++ html/search/all_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['isprotocolactive',['isProtocolActive',['../class_adafruit___c_h224.html#ab49d59d88d28abbc3cd20882dd2e143c',1,'Adafruit_CH224']]]
+  ['isprotocolactive',['isProtocolActive',['../class_adafruit___c_h224.html#ab49d59d88d28abbc3cd20882dd2e143c',1,'Adafruit_CH224']]],
+  ['isvoltageavailable',['isVoltageAvailable',['../class_adafruit___c_h224.html#aca15269787a7481ed428258ee65688a9',1,'Adafruit_CH224']]]
 ];
